f2b11f2f20fdd0b7ba449d77fe8a15da001d79af
nguyen duy son <nguyenduyson60@gmail.com>
2021-03-11 09:58:16 +0700

    register

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "tour#index"
+    get "/signup", to: "users#new"
     resources :tours, only: %i("index")
+    resources :users, only: %i("new create ")
   end
 end
